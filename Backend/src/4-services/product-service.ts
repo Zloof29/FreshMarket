540bf3370ca371b8794c5b57ff206dfa2991cd4a
Lior Bas <look551@gmail.com>
2024-12-17 18:52:21 +0200

    added functions

--- Backend/src/4-services/product-service.ts
+++ Backend/src/4-services/product-service.ts
@@ -1,10 +1,44 @@
+import { fileSaver } from "uploaded-file-saver";
+import { dal } from "../2-utils/dal";
+import { ProductModel } from "../3-models/productModel";
+import { OkPacketParams } from "mysql2";
+
 // Product service - any logic regarding products:
 class ProductService {
+  // Get all products:
+  public async getAllProducts(userId: number): Promise<ProductModel[]> {
+    const sql =
+      'SELECT *, CONCAT("http://localhost:400/api/products/images/", imageName) as imageUrl FROM products WHERE userId = ?';
 
-    // Get all products: 
-    public async getAllProducts() {
+    const products: ProductModel[] = await dal.execute(sql, [userId]);
 
+    return products;
+  }
 
+  public async addProduct(product: ProductModel): Promise<ProductModel> {
+    const imageName = product.imageName
+      ? await fileSaver.add(product.imageName)
+      : null;
+
+    const sql =
+      "INSERT INTO products (id, productCode, imageName, quantityPerBox, orderByBox, orderByWeight, price, userId) VALUES (default, ?, ?, ?, ?, ?, ?, ?, ?";
+
+    const values = [
+      product.productCode,
+      imageName,
+      product.quantityPerBox,
+      product.orderByBox,
+      product.orderByWeight,
+      product.price,
+      product.userId,
+    ];
+
+    const info: OkPacketParams = await dal.execute(sql, values);
+
+    product.id - info.insertId;
+
+    return product;
+  }
 }
 
 export const productService = new ProductService();
