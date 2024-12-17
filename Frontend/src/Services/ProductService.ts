f07a9d1108eb95a5d8b42d4021b9adab7ed4f714
Lior Bas <look551@gmail.com>
2024-12-17 19:37:43 +0200

    added functions and redux

--- Frontend/src/Services/ProductService.ts
+++ Frontend/src/Services/ProductService.ts
@@ -1,4 +1,40 @@
+import axios, { AxiosRequestConfig } from "axios";
+import { ProductModel } from "../Models/productModel";
+import { appConfig } from "../Utils/AppConfig";
+import { productActions, store } from "../Redux/store";
+
 class ProductService {
+  public async getAllProducts(userId: number): Promise<ProductModel[]> {
+    const response = await axios.get<ProductModel[]>(
+      appConfig.productsUrl + userId
+    );
+
+    const products: ProductModel[] = response.data;
+
+    const action = productActions.initProducts(products);
+    store.dispatch(action);
+
+    return products;
+  }
+
+  public async addProduct(product: ProductModel): Promise<ProductModel> {
+    const options: AxiosRequestConfig = {
+      headers: { "Content-Type": "multipart/form-data" },
+    };
+
+    const response = await axios.post<ProductModel>(
+      appConfig.productsUrl,
+      product,
+      options
+    );
+
+    const addedProduct = response.data;
+
+    const action = productActions.addProduct(addedProduct);
+    store.dispatch(action);
+
+    return addedProduct;
+  }
 }
 
 export const productService = new ProductService();
